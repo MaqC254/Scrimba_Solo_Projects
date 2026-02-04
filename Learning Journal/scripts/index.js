db88dc98ef9681f972eed00da7b334d242ebbf66
MaqC <maxwellm751@gmail.com>
2026-02-04 13:42:06 +0300

    Bigger screens done. Need to do more visual styling

--- Learning Journal/scripts/index.js
+++ Learning Journal/scripts/index.js
@@ -4,10 +4,14 @@ document.addEventListener('click',function(e){
     if(e.target.parentNode.className === "blog-item"){
         location.href = "./blog-content.html";
     }else if(e.target.className === "view-more"){
-        const hiddenDiv = document.getElementById("hidden-blog-items")
+        const blog4 = document.getElementById("blog-4")
+        const blog5 = document.getElementById("blog-5")
+        const blog6 = document.getElementById("blog-6")
         const viewMoreBtn = document.getElementById("view-more")
-        console.log(hiddenDiv)
-        hiddenDiv.style.display = "block";
+
+        blog4.style.display = "block";
+        blog5.style.display = "block";
+        blog6.style.display = "block";
         viewMoreBtn.style.display = "none";
 
     }
